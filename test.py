39d227daaec4afcffffd8278a2bf767d2a84ee74
Tom MacWright <tom@macwright.org>
2016-06-08 14:52:30 -0400

    Fix encode

--- test.py
+++ test.py
@@ -9,18 +9,18 @@ libraries = [
 
 class TestPolylineMethods(unittest.TestCase):
 
-    def test_forward(self):
+    def test_encode(self):
         for library in libraries:
             self.assertEqual(
                 subprocess.check_output([
-                    os.path.join(library, "forward"),
+                    os.path.join(library, "encode"),
                 "[[1,2],[3,4]]"]), "_ibE_seK_seK_seK")
 
-    def test_reverse(self):
+    def test_decode(self):
         for library in libraries:
             self.assertEqual(
                 subprocess.check_output([
-                    os.path.join(library, "reverse"),
+                    os.path.join(library, "decode"),
                 "_ibE_seK_seK_seK"]), "[[1,2],[3,4]]")
 
 if __name__ == '__main__':
